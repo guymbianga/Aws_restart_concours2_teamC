0cc85b0989e977c758fcb496e31d6203b6734ad9
Bekono Roland <bekonrolang@gmail.com>
2022-03-01 01:30:40 +0100

    Définition des modeles de dictionnaiare de l'app

---
+++ defModel.py
@@ -0,0 +1,14 @@
+modelClient = {
+    "nom":None,
+    "telephone":None
+}
+modelProduit = {
+    "nom":None,
+    "prix":0,
+     "quantite":0
+}
+modelCommande = {
+    "client":None,
+    "produits":[],
+    "dateCommande":None
+}
